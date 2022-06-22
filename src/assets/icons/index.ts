2a41166b396a37b5501ccd40c5893cc98daee7a6
Maksym <max.sharinov@gmail.com>
2022-06-22 20:40:41 +0300

    Added table reservation button

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,2 @@
+export { default as ReservationIcon } from './reservation.png'
+export { default as ReservationDarkIcon } from './reservation-dark.png'
--- src/screens/Home/components/ReserveTable.tsx
+++ src/screens/Home/components/ReserveTable.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Image, TouchableOpacity } from 'react-native'
 
 import { Box, Text } from 'components'
-import { TouchableOpacity } from 'react-native'
+import { ReservationIcon } from 'assets/icons'
 
 const Button = styled(Box)`
   width: 100%;
@@ -10,14 +11,25 @@ const Button = styled(Box)`
   border-radius: ${({ theme }) => theme.spacing.m}px;
 `
 
+const Content = styled(TouchableOpacity)`
+  width: 100%;
+  flex-direction: row;
+`
+
 const ReserveTable = () => {
   return (
     <Button marginVertical='xxl' paddingTop='l' paddingHorizontal='l' backgroundColor='default'>
-      <TouchableOpacity hitSlop={{ top: 20, bottom: 20, left: 10, right: 10 }}>
-        <Text variant='h5' textTransform='uppercase'>
-          Reserve a table
-        </Text>
-      </TouchableOpacity>
+      <Content hitSlop={{ top: 48, bottom: 24, left: 32, right: 32 }}>
+        <Image source={ReservationIcon} />
+        <Box marginLeft='m'>
+          <Text variant='h5' textTransform='uppercase'>
+            Reserve a table
+          </Text>
+          <Text variant='body2' color='p2'>
+            book a table in the app and get points automatically
+          </Text>
+        </Box>
+      </Content>
     </Button>
   )
 }
