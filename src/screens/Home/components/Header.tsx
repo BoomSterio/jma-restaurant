ff0682d374b88704353382ef300c0ce7c99eaba0
Maksym <max.sharinov@gmail.com>
2022-06-23 13:48:30 +0300

    Centered greetings in header

--- src/screens/Home/components/Header.tsx
+++ src/screens/Home/components/Header.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { View } from 'react-native'
 import styled from 'styled-components'
 
-import { Text } from 'components'
+import { Box, Text } from 'components'
 import ScanIcon from './ScanIcon'
 
-const Wrapper = styled(View)`
+const Wrapper = styled(Box)`
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
@@ -14,21 +13,32 @@ const Wrapper = styled(View)`
   padding-vertical: ${({ theme }) => theme.spacing.xl}px;
   padding-horizontal: ${({ theme }) => theme.spacing.m}px;
 `
-const Actions = styled(View)`
+const Actions = styled(Box)`
   flex-direction: row;
   align-items: center;
+`
+
+const Greetings = styled(Box)`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  bottom: 0px;
+  right: 0px;
+  align-items: center;
+  justify-content: flex-end;
 `
 
 const Header = () => {
   return (
     <Wrapper>
       <Text variant='logo'>PDF™</Text>
-      <Text variant='h6' style={{ alignSelf: 'flex-end' }} textTransform='uppercase'>
-        Hi Anna
-      </Text>
+      <Greetings paddingBottom='xl'>
+        <Text variant='h6' textTransform='uppercase'>
+          Hi Anna
+        </Text>
+      </Greetings>
       <Actions>
         <ScanIcon />
-        {/* <ScanIcon /> */}
       </Actions>
     </Wrapper>
   )
